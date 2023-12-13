39d56b58399800d20c431b7b37997486526d3c17
Maaldobo <maaldobo55@gmail.com>
2023-12-12 19:58:54 -0600

    listo

---
+++ ProgramacionBasica/practica01_console.js
@@ -0,0 +1,11 @@
+console.log("hola mundo");
+
+console.log("Grupo 1 : Contador")
+
+console.count("contando");
+console.count("contando");
+console.count("contando");
+
+console.log("Grupo 2 : Tipos de mensajes");
+console.warn("Advertencia");
+console.error("Error")
